cd87f9f3edc6df443df9500e3184aa94172d3700
Harry Kwon <harrykwon20@gmail.com>
2019-01-27 04:08:13 -0500

    created async rmdir

--- recursive-rmdir.js
+++ recursive-rmdir.js
@@ -2,28 +2,53 @@ var fs = require('fs');
 var path = require('path');
     
 
-/*
-//unsync draft, need semaphore?
-function recursiveRmdir(directory){
+//async rmdir
+var rmdir = function(directory, callback) {
+  switch(arguments.length){
+    case 1: callback=function(){};
+  }
+  
   console.log('recursively removing directory: ' + directory);
-  //fs.readdir lists all files in directory
+  
   fs.readdir(directory, function(err, files){
-    for(const file of files){
-      let filePath = path.join(directory, file);
+    var count=0;
+    for(const file of files) {
+      count++;
+      var on_remove = function(){
+        count--;
+        console.log(directory + ": " + count);
+        if(count === 0) {
+        	fs.rmdir(directory, callback);
+        }
+      }
+      
+      var filePath = path.join(directory, file);
+      console.log(filePath);
       fs.stat(filePath, function(err, stats){
         if(stats.isDirectory()){
           //recursively remove directory
-          recursiveRmdir(filePath);
+          rmdir(filePath, on_remove);
         } else {
           //remove file
-          console.log('removing file: ' + filePath);
-          fs.unlink(filePath, function(err) {
-                console.log('removed file: ' + filePath);
+          fs.unlink(filePath, function(err){
+            on_remove();
+          	console.log('removed file: ' + filePath);
           });
         }
       });
     }
   });
+}
+
+/*
+//async test
+try {
+  fs.mkdirSync('foo');
+  fs.mkdirSync('./foo/bar');
+  fs.writeFileSync('./foo/bar/baz.txt', 'yellow submarine');
+  rmdir('foo');
+} catch(e) {
+  console.log('async test failed');
 }
 */
 
@@ -47,14 +72,16 @@ var rmdirSync= function (directory){
   console.log('recusively removed directory: ' + directory);
 }
 
-//create test directory
+//sync test
+/*
 try {
   fs.mkdirSync('foo');
   fs.mkdirSync('./foo/bar');
   fs.writeFileSync('./foo/bar/baz.txt', 'yellow submarine');
   rmdirSync('foo');
 } catch(e) {
-    //
+  console.log('sync test failed');
 }
+*/
 
 module.exports.rmdirSync = rmdirSync;
